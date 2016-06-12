15e3e2aed3fc37b81ae9d02f5a8be3e6a2fbeeef
skriklgb <1071282757@qq.com>
2016-06-12 09:20:14 +0800

    splash布局

--- app/src/main/java/skrik/lgb/mobilesafe/activity/SplashActivity.java
+++ app/src/main/java/skrik/lgb/mobilesafe/activity/SplashActivity.java
@@ -1,13 +1,16 @@
-package skrik.lgb.mobilesafe;
+package skrik.lgb.mobilesafe.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+import skrik.lgb.mobilesafe.R;
+
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
     }
 }
