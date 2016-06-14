b4bf27cc493ebd85274e984e946ee276b71966d0
skriklgb <1071282757@qq.com>
2016-06-14 22:21:25 +0800

    2-5,九宫格使用 模块：主界面 目的：主界面布局,实现九宫格 1.GridView使用-->和ListView使用方式类似-->数据适配器一致的 2.数据适配器的使用
    错误调试：
    .lang.NullPointerException
        at skrik.lgb.mobilesafe.activity.HomeActivity$MyAdapter.getView(HomeActivity.java:68)
    
          TextView tv_title = (TextView) view.findViewById(R.id.tv_title);
               ImageView iv_icon = (ImageView) view.findViewById(R.id.iv_icon);

--- app/src/main/java/skrik/lgb/mobilesafe/activity/HomeActivity.java
+++ app/src/main/java/skrik/lgb/mobilesafe/activity/HomeActivity.java
@@ -2,14 +2,78 @@ package skrik.lgb.mobilesafe.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import skrik.lgb.mobilesafe.R;
 
 public class HomeActivity extends Activity {
 
+    private GridView mGv_home;
+    private String[] mTitleStr;
+    private int[] mDrawableIds;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
+        initUI();
+        //初始化数据的方法
+        initData();
+    }
+
+    private void initData() {
+        //准备数据(文字(9组),图片(9张))
+        mTitleStr = new String[ ]{
+                  "手机防盗","通信卫士","软件管理","进程管理","流量统计","手机杀毒","缓存清理","高级工具","设置中心"
+          };
+        mDrawableIds = new int[]{
+                  R.drawable.home_safe,R.drawable.home_callmsgsafe,
+                  R.drawable.home_apps,R.drawable.home_taskmanager,
+                  R.drawable.home_netmanager,R.drawable.home_trojan,
+                  R.drawable.home_sysoptimize,R.drawable.home_tools,R.drawable.home_settings
+          };
+        //九宫格控件设置数据适配器(等同ListView数据适配器)
+        mGv_home.setAdapter(new MyAdapter());
+    }
+
+    class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {      //得到数据的行数
+            //条目的总数	文字组数 == 图片张数
+            return mTitleStr.length;
+        }
+
+        @Override
+        public Object getItem(int position) {  //根据position得到某一行的记录
+            return mTitleStr[position];
+        }
+
+        @Override
+        public long getItemId(int position) { //得到某一条记录的ID
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {  //我们通常会给它写个布局文件
+            View view = View.inflate(getApplicationContext(),R.layout.gridview_item, null);
+           TextView tv_title = (TextView) view.findViewById(R.id.tv_title);
+           ImageView iv_icon = (ImageView) view.findViewById(R.id.iv_icon);
+            tv_title.setText(mTitleStr[position]);
+            iv_icon.setBackgroundResource(mDrawableIds[position]);
+            return view;
+        }
+    }
+
+
+    private void initUI() {
+        mGv_home = (GridView) findViewById(R.id.gv_home);
+
     }
 }
