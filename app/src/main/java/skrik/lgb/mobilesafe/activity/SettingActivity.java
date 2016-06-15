1c5e3000e3704fa141d15eeafbb563abe300a806
skriklgb <1071282757@qq.com>
2016-06-15 20:13:58 +0800

    2-8,自定义组合控件中--相关方法--实现点击后选中与不选中切换 2-9,选中SettingItemView条目状态切换 模块：设置中心 目的：自定义组合控件中的相关方法，实现点击后选中与不选中切换 1.文字（TextView）与选择框（CkeckBox）的绑定    checkBox是否选中,决定SettingItemView是否开启,isCheck(){return checkbox.isCheck()}方法
    2.内部类使用外部变量，加final

--- app/src/main/java/skrik/lgb/mobilesafe/activity/SettingActivity.java
+++ app/src/main/java/skrik/lgb/mobilesafe/activity/SettingActivity.java
@@ -2,8 +2,10 @@ package skrik.lgb.mobilesafe.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import skrik.lgb.mobilesafe.R;
+import skrik.lgb.mobilesafe.view.SettingItemView;
 
 public class SettingActivity extends Activity {
 
@@ -11,5 +13,22 @@ public class SettingActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        initUpdate();
+    }
+
+    private void initUpdate() {
+        final SettingItemView siv_update  = (SettingItemView) findViewById(R.id.siv_update);
+        siv_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //如果之前是选中的,点击过后,变成未选中
+                //如果之前是未选中的,点击过后,变成选中
+                //获取之前的选中状态
+               boolean isCheck = siv_update.isCheck();
+                //将原有状态取反,等同上诉的两部操作
+                siv_update.setCheck(!isCheck);
+            }
+        });
     }
 }
--- app/src/main/java/skrik/lgb/mobilesafe/view/SettingItemView.java
+++ app/src/main/java/skrik/lgb/mobilesafe/view/SettingItemView.java
@@ -10,6 +10,10 @@ import android.widget.TextView;
 import skrik.lgb.mobilesafe.R;
 
 public class SettingItemView extends RelativeLayout {
+
+    private CheckBox mCb_setting_box;
+    private TextView mTv_setting_des;
+
     public SettingItemView(Context context) {
         this(context,null);
     }
@@ -26,9 +30,31 @@ public class SettingItemView extends RelativeLayout {
 		/*View view = View.inflate(context, R.layout.setting_item_view, null);
 		this.addView(view);*/
         TextView  tv_setting_titile  = (TextView) findViewById(R.id.tv_setting_titile);
-        TextView  tv_setting_des  = (TextView) findViewById(R.id.tv_setting_des);
-        CheckBox  cb_setting_box   = (CheckBox) findViewById(R.id.cb_setting_box);
+        mTv_setting_des = (TextView) findViewById(R.id.tv_setting_des);
+        mCb_setting_box = (CheckBox) findViewById(R.id.cb_setting_box);
     }
 
+    /**
+     * 判断是否开启的方法
+     * @return  返回当前SettingItemView是否选中状态	true开启(checkBox返回true)	false关闭(checkBox返回false)
+     */
+    public boolean isCheck(){
+        //由checkBox的选中结果,决定当前条目是否开启
+            return mCb_setting_box.isChecked();
+    }
+
+    /**
+     *
+     * @param isCheck 作为是否开启的变量,由点击过程中去做传递
+     */
+    public void setCheck(boolean isCheck){
+        //当前条目在选择的过程中,mCb_setting_box选中状态也在跟随(isCheck)变化
+            mCb_setting_box.setChecked(isCheck);
+        if (isCheck){
+            mTv_setting_des.setText("自动更新已开启");
+        } else {
+            mTv_setting_des.setText("自动更新已关闭");
+        }
+    }
 
 }
