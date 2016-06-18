3dc049f1f83c18d52cbc53544ce0d05ff9d02396
skriklgb <1071282757@qq.com>
2016-06-18 21:15:40 +0800

    3-10,导航界面和功能列表界面跳转逻辑处理 模块：手机防盗模块 目的：完成导航界面的布局

--- app/src/main/java/skrik/lgb/mobilesafe/activity/Setup3Activity.java
+++ app/src/main/java/skrik/lgb/mobilesafe/activity/Setup3Activity.java
@@ -7,23 +7,23 @@ import android.view.View;
 
 import skrik.lgb.mobilesafe.R;
 
-public class Setup2Activity extends Activity {
+public class Setup3Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup2);
+        setContentView(R.layout.activity_setup3);
     }
 
     public void  nextPage(View view){
-        Intent intent = new Intent(getApplicationContext(), Setup3Activity.class);
+        Intent intent = new Intent(getApplicationContext(), Setup4Activity.class);
         startActivity(intent);
 
         finish();
     }
 
     public void  prePage(View view){
-        Intent intent = new Intent(getApplicationContext(), Setup1Activity.class);
+        Intent intent = new Intent(getApplicationContext(), Setup2Activity.class);
         startActivity(intent);
 
         finish();
--- app/src/main/java/skrik/lgb/mobilesafe/activity/Setup4Activity.java
+++ app/src/main/java/skrik/lgb/mobilesafe/activity/Setup4Activity.java
@@ -6,26 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 
 import skrik.lgb.mobilesafe.R;
+import skrik.lgb.mobilesafe.utils.ConstantValue;
+import skrik.lgb.mobilesafe.utils.SpUtil;
 
-public class Setup2Activity extends Activity {
+public class Setup4Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup2);
+        setContentView(R.layout.activity_setup4);
     }
 
     public void  nextPage(View view){
+        Intent intent = new Intent(getApplicationContext(), SetupOverActivity.class);
+        startActivity(intent);
+
+        finish();
+        SpUtil.putBoolean(this, ConstantValue.SETUP_OVER,true);
+    }
+
+    public void  prePage(View view){
         Intent intent = new Intent(getApplicationContext(), Setup3Activity.class);
         startActivity(intent);
 
         finish();
     }
 
-    public void  prePage(View view){
-        Intent intent = new Intent(getApplicationContext(), Setup1Activity.class);
-        startActivity(intent);
-
-        finish();
-    }
 }
