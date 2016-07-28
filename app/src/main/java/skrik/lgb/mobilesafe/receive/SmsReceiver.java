30689373e399aa3a4e669ccf08abbcac9706bdeb
skriklgb <1071282757@qq.com>
2016-07-28 21:22:40 +0800

    5-2，接收短信，播放音乐

---
+++ app/src/main/java/skrik/lgb/mobilesafe/receive/SmsReceiver.java
@@ -0,0 +1,43 @@
+package skrik.lgb.mobilesafe.receive;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.telephony.SmsMessage;
+
+import skrik.lgb.mobilesafe.R;
+import skrik.lgb.mobilesafe.utils.ConstantValue;
+import skrik.lgb.mobilesafe.utils.SpUtil;
+
+public class SmsReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //1,判断是否开启了防盗保护
+        boolean open_security = SpUtil.getBoolean(context, ConstantValue.OPEN_SECURITY, false);
+        if (open_security){
+            //2,获取短信内容
+            Object[] objects = (Object[]) intent.getExtras().get("pdus");
+            //3,循环遍历短信过程
+            for (Object object : objects){
+                //4,获取短信对象
+                SmsMessage sms = SmsMessage.createFromPdu((byte[]) object);
+                String originatingAddress = sms.getOriginatingAddress();
+                String messageBody = sms.getMessageBody();
+
+                //6,判断是否包含播放音乐的关键字
+                if (messageBody.contains("#*alarm*#")){
+                    //7,播放音乐(准备音乐,MediaPlayer)
+                    MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.ylzs);
+                    mediaPlayer.setLooping(true);
+                    mediaPlayer.start();
+                }
+            }
+
+
+        }
+
+    }
+}
